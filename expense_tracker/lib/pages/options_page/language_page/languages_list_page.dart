8acc429320b297a84fc7ee0a18eb53ec3de8e22e
ITASerus <decrecchio.ernesto@gmail.com>
2023-04-25 21:40:57 +0200

    Added flags to locale list options

--- expense_tracker/lib/pages/options_page/language_page/languages_list_page.dart
+++ expense_tracker/lib/pages/options_page/language_page/languages_list_page.dart
@@ -1,7 +1,7 @@
-import 'package:expense_tracker/l10n/l10n.dart';
 import 'package:expense_tracker/notifiers/locale_provider.dart';
 import 'package:expense_tracker/style.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
@@ -31,14 +31,46 @@ class _LanguagesListPageState extends State<LanguagesListPage> {
     return ListView(
       children: [
         ListTile(
-          title: const Text('English'),
+          title: const Text('English (UK)'),
           onTap: () => Provider.of<LocaleProvider>(context, listen: false)
               .setLocale(const Locale('en')),
+          leading: Container(
+            clipBehavior: Clip.antiAlias,
+            height: 30,
+            width: 30,
+            decoration: const BoxDecoration(shape: BoxShape.circle),
+            child: SvgPicture.asset(
+              'assets/flags/United-Kingdom.svg',
+              fit: BoxFit.cover,
+            ),
+          ),
+          trailing: Provider.of<LocaleProvider>(context, listen: true)
+                      .locale
+                      ?.languageCode ==
+                  'en'
+              ? const Icon(Icons.check)
+              : null,
         ),
         ListTile(
           title: const Text('Italiano'),
           onTap: () => Provider.of<LocaleProvider>(context, listen: false)
               .setLocale(const Locale('it')),
+          leading: Container(
+            clipBehavior: Clip.antiAlias,
+            height: 30,
+            width: 30,
+            decoration: const BoxDecoration(shape: BoxShape.circle),
+            child: SvgPicture.asset(
+              'assets/flags/Italy.svg',
+              fit: BoxFit.cover,
+            ),
+          ),
+          trailing: Provider.of<LocaleProvider>(context, listen: true)
+                      .locale
+                      ?.languageCode ==
+                  'it'
+              ? const Icon(Icons.check)
+              : null,
         ),
       ],
     );
