74c630ebb520f85b72d18db0f63f9adeb8834c90
Ernesto De Crecchio <decrecchio.ernesto@gmail.com>
2023-05-22 12:40:50 +0200

    Feat/Select by date option in account details (#1)
    * Added date filter
    
    * fixed currentWeekLastDay func
    
    * Changed Intl locale setting
    
    * Code cleanup
    
    * Fixed time picker size
    
    * Fixed labels and button sizes

--- expense_tracker/lib/pages/common/custom_modal_bottom_sheet.dart
+++ expense_tracker/lib/pages/common/custom_modal_bottom_sheet.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
-showCustomModalBottomSheet(
-    {required BuildContext context,
-    required Widget Function(BuildContext) builder}) async {
+showCustomModalBottomSheet({
+  required BuildContext context,
+  required Widget Function(BuildContext) builder,
+}) async {
   await showModalBottomSheet(
       shape: const RoundedRectangleBorder(
         borderRadius: BorderRadius.only(
