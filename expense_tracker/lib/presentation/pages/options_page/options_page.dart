748b2bd636ef3e7e918cec47843898a37724675e
Ernesto De Crecchio <decrecchio.ernesto@gmail.com>
2026-02-19 19:32:29 +0100

    Improve options list page

--- expense_tracker/lib/presentation/pages/options_page/options_page.dart
+++ expense_tracker/lib/presentation/pages/options_page/options_page.dart
@@ -14,7 +14,6 @@ import 'package:expense_tracker/presentation/pages/options_page/notification_pag
 import 'package:expense_tracker/style.dart';
 import 'package:expense_tracker/presentation/pages/update_history_page/update_history_page.dart';
 import 'package:flutter/material.dart';
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:in_app_review/in_app_review.dart';
 
@@ -44,112 +43,63 @@ class OptionsPage extends ConsumerWidget {
     return SingleChildScrollView(
       child: Column(
         children: [
-          ListTile(
-            leading: const SizedBox(
-              height: double.infinity,
-              child: Icon(
-                Icons.grid_view_rounded,
-                color: CustomColors.darkBlue,
-              ),
-            ),
-            title: Text(appLocalizations.categories),
-            subtitle: Text(appLocalizations.categoriesOptionDescription),
-            trailing: const Icon(Icons.chevron_right_rounded),
+          OptionListTile(
+            title: appLocalizations.categories,
+            subtitle: appLocalizations.categoriesOptionDescription,
+            leadingIcon: Icons.grid_view_rounded,
             onTap: () =>
                 Navigator.of(context).pushNamed(CategoriesListPage.routeName),
           ),
           const Divider(),
-          ListTile(
-            leading: const SizedBox(
-              height: double.infinity,
-              child: Icon(
-                Icons.account_balance_rounded,
-                color: CustomColors.darkBlue,
-              ),
-            ),
-            title: Text(appLocalizations.accounts),
-            subtitle: Text(appLocalizations.accountsOptionDescription),
-            trailing: const Icon(Icons.chevron_right_rounded),
+          OptionListTile(
+            title: appLocalizations.accounts,
+            subtitle: appLocalizations.accountsOptionDescription,
+            leadingIcon: Icons.account_balance_rounded,
             onTap: () =>
                 Navigator.of(context).pushNamed(AccountsListPage.routeName),
           ),
           const Divider(),
-          ListTile(
-            leading: const SizedBox(
-              height: double.infinity,
-              child: Icon(
-                Icons.translate_rounded,
-                color: CustomColors.darkBlue,
-              ),
-            ),
-            title: Text(appLocalizations.language),
-            subtitle: Text(appLocalizations.languageOptionDescription),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                if (currentLocale != null) Text(currentLocale.languageCode),
-                const Icon(Icons.chevron_right_rounded),
-              ],
-            ),
+          OptionListTile(
+            title: appLocalizations.language,
+            subtitle: appLocalizations.languageOptionDescription,
+            leadingIcon: Icons.translate_rounded,
+            trailingWidgets: [
+              if (currentLocale != null) Text(currentLocale.languageCode)
+            ],
             onTap: () =>
                 Navigator.of(context).pushNamed(LanguagesListPage.routeName),
           ),
           const Divider(),
-          ListTile(
-            leading: const SizedBox(
-              height: double.infinity,
-              child: Icon(
-                Icons.currency_exchange_outlined,
-                color: CustomColors.darkBlue,
-              ),
-            ),
-            title: Text(appLocalizations.currency),
-            subtitle: Text(appLocalizations.selectCurrency),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                if (currentCurrency != null) Text(currentCurrency.symbolNative),
-                const Icon(Icons.chevron_right_rounded),
-              ],
-            ),
+          OptionListTile(
+            title: appLocalizations.currency,
+            subtitle: appLocalizations.selectCurrency,
+            leadingIcon: Icons.currency_exchange_outlined,
+            trailingWidgets: [
+              if (currentCurrency != null) Text(currentCurrency.symbolNative),
+            ],
             onTap: () =>
                 Navigator.of(context).pushNamed(CurrencyPage.routeName),
           ),
           const Divider(),
-          ListTile(
-            leading: const SizedBox(
-              height: double.infinity,
-              child: Icon(
-                Icons.notifications_active_rounded,
-                color: CustomColors.darkBlue,
-              ),
-            ),
-            title: Text(appLocalizations.reminder),
-            subtitle: Text(appLocalizations.reminderOptionDescription),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(ref.watch(notificationsEnabledProvider) != null &&
-                        ref.watch(notificationsEnabledProvider) == true
-                    ? appLocalizations.yes
-                    : appLocalizations.no),
-                const Icon(Icons.chevron_right_rounded),
-              ],
-            ),
+          OptionListTile(
+            title: appLocalizations.reminder,
+            subtitle: appLocalizations.reminderOptionDescription,
+            leadingIcon: Icons.notifications_active_rounded,
+            trailingWidgets: [
+              Text(ref.watch(notificationsEnabledProvider) != null &&
+                      ref.watch(notificationsEnabledProvider) == true
+                  ? appLocalizations.yes
+                  : appLocalizations.no),
+            ],
             onTap: () =>
                 Navigator.of(context).pushNamed(ReminderPage.routeName),
           ),
           const Divider(),
-          ListTile(
-            leading: const SizedBox(
-              height: double.infinity,
-              child: Icon(
-                Icons.star_rounded,
-                color: CustomColors.darkBlue,
-              ),
-            ),
-            title: Text(appLocalizations.feedback),
-            subtitle: Text(appLocalizations.feedbackAndReviewOptionDescription),
+          OptionListTile(
+            title: appLocalizations.feedback,
+            subtitle: appLocalizations.feedbackAndReviewOptionDescription,
+            leadingIcon: Icons.star_rounded,
+            enableRightArrow: false,
             onTap: () {
               final InAppReview inAppReview = InAppReview.instance;
               inAppReview.openStoreListing(
@@ -158,47 +108,26 @@ class OptionsPage extends ConsumerWidget {
             },
           ),
           const Divider(),
-          ListTile(
-            leading: const SizedBox(
-              height: double.infinity,
-              child: Icon(
-                Icons.auto_awesome_rounded,
-                color: CustomColors.darkBlue,
-              ),
-            ),
-            title: Text(appLocalizations.updateHistory),
-            subtitle: Text(appLocalizations.updateHistoryOptionDescription),
-            trailing: const Icon(Icons.chevron_right_rounded),
+          OptionListTile(
+            title: appLocalizations.updateHistory,
+            subtitle: appLocalizations.updateHistoryOptionDescription,
+            leadingIcon: Icons.auto_awesome_rounded,
             onTap: () =>
                 Navigator.of(context).pushNamed(UpdateHistoryPage.routeName),
           ),
           const Divider(),
-          ListTile(
-            leading: const SizedBox(
-              height: double.infinity,
-              child: Icon(
-                Icons.analytics_outlined,
-                color: CustomColors.darkBlue,
-              ),
-            ),
-            title: Text(appLocalizations.analytics),
-            subtitle: Text(appLocalizations.analyticsOptionDescription),
-            trailing: const Icon(Icons.chevron_right_rounded),
+          OptionListTile(
+            title: appLocalizations.analytics,
+            subtitle: appLocalizations.analyticsOptionDescription,
+            leadingIcon: Icons.analytics_outlined,
             onTap: () => Navigator.of(context)
                 .pushNamed(DiagnosticsSettingsPage.routeName),
           ),
           const Divider(),
-          ListTile(
-            leading: const SizedBox(
-              height: double.infinity,
-              child: Icon(
-                Icons.info_outline,
-                color: CustomColors.darkBlue,
-              ),
-            ),
-            title: Text(appLocalizations.info),
-            subtitle: Text(appLocalizations.infoOptionDescription),
-            trailing: const Icon(Icons.chevron_right_rounded),
+          OptionListTile(
+            title: appLocalizations.info,
+            subtitle: appLocalizations.infoOptionDescription,
+            leadingIcon: Icons.info_outline,
             onTap: () => Navigator.of(context).pushNamed(AboutPage.routeName),
           ),
           const Divider(),
@@ -221,6 +150,52 @@ class OptionsPage extends ConsumerWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class OptionListTile extends StatelessWidget {
+  final String title;
+  final String? subtitle;
+  final IconData leadingIcon;
+  final bool enableRightArrow;
+  final List<Widget>? trailingWidgets;
+  final Function()? onTap;
+
+  const OptionListTile({
+    super.key,
+    required this.title,
+    this.subtitle,
+    required this.leadingIcon,
+    this.onTap,
+    this.enableRightArrow = true,
+    this.trailingWidgets,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Widget> trailingChildren = [
+      if (trailingWidgets != null) ...trailingWidgets!,
+      if (enableRightArrow) const Icon(Icons.chevron_right_rounded),
+    ];
+
+    return ListTile(
+      leading: SizedBox(
+        height: double.infinity,
+        child: Icon(
+          leadingIcon,
+          color: CustomColors.darkBlue,
+        ),
+      ),
+      title: Text(title),
+      subtitle: subtitle != null ? Text(subtitle!) : null,
+      trailing: trailingChildren.isNotEmpty
+          ? Row(
+              mainAxisSize: MainAxisSize.min,
+              children: trailingChildren,
+            )
+          : null,
+      onTap: onTap,
     );
   }
 }
